eb3b4b10714661dd31b0ffc2f749a57e10933576
Nishant Rodrigues <nishantjr@gmail.com>
2024-11-14 11:50:59 -0500

    Implement evaluation for Int expressions

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+data AExp = Int Int
+          | X | Y -- Variables
+          | Add AExp AExp
+    deriving Show
+
+eval :: Int -> Int -> AExp -> Int
+eval _ _ (Int n)    = n
+eval x y (Add n m)  = (eval x y n) + (eval x y m)
+eval x _ X    = x
+eval _ y Y    = y
+
+test_eval =     (eval 2 2  (Int 42)        == 42)
+             && (eval 2 42 Y               == 42)
+             && (eval 2 40 (Add (Int 2) Y) == 42)
+             && (eval 2 40 (Add X       Y) == 42)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putStrLn $ show test_eval
