f6b5e3cf38e443b89f92f7d8f7754948a4ceec49
Nishant Rodrigues <nishantjr@gmail.com>
2024-11-14 12:07:09 -0500

    Implement evaluation for Bool expressions

--- src/Main.hs
+++ src/Main.hs
@@ -5,16 +5,31 @@ data AExp = Int Int
           | Add AExp AExp
     deriving Show
 
-eval :: Int -> Int -> AExp -> Int
-eval _ _ (Int n)    = n
-eval x y (Add n m)  = (eval x y n) + (eval x y m)
-eval x _ X    = x
-eval _ y Y    = y
+evalA :: Int -> Int -> AExp -> Int
+evalA _ _ (Int n)    = n
+evalA x y (Add n m)  = (evalA x y n) + (evalA x y m)
+evalA x _ X    = x
+evalA _ y Y    = y
 
-test_eval =     (eval 2 2  (Int 42)        == 42)
-             && (eval 2 42 Y               == 42)
-             && (eval 2 40 (Add (Int 2) Y) == 42)
-             && (eval 2 40 (Add X       Y) == 42)
+test_evalA =    (evalA 2 2  (Int 42)        == 42)
+             && (evalA 2 42 Y               == 42)
+             && (evalA 2 40 (Add (Int 2) Y) == 42)
+             && (evalA 2 40 (Add X       Y) == 42)
+
+
+data BExp = Bool Bool
+          | LessThan AExp AExp
+
+evalB :: Int -> Int -> BExp -> Bool
+evalB _ _ (Bool b)       = b
+evalB x y (LessThan l r) = (evalA x y l) < (evalA x y r)
+
+test_evalB =    (evalB 2 2  (Bool True)     == True)
+             && (evalB 2 2  (LessThan X Y)  == False)
+             && (evalB 2 3  (LessThan X Y)  == True)
+             && (evalB 3 2  (LessThan X Y)  == False)
+
 
 main :: IO ()
-main = putStrLn $ show test_eval
+main = do putStrLn $ "test_evalA: " ++ (show test_evalA)
+          putStrLn $ "test_evalB: " ++ (show test_evalB)
