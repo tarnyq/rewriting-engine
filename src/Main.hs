333281119bd19148f70dd12502dce69bb01c6096
Nishant Rodrigues <nishantjr@gmail.com>
2024-11-16 22:20:29 -0500

    Support non-determinism via the List Monad

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,7 @@ import Control.Monad.State
 -- Configuration
 
 type Env = (Int, Int)
-type Imp = State Env
+type Imp = StateT Env []
 
 
 -------------------------------------------------------------------------------
@@ -74,38 +74,38 @@ runStmt (While cond stmts) = do c <- runB cond
 -------------------------------------------------------------------------------
 -- Testing
 
-evalA :: AExp -> Env -> Int
-evalA exp env = evalState (runA exp) env
+evalA :: AExp -> Env -> [Int]
+evalA exp env = evalStateT (runA exp) env
 
-evalB :: BExp -> Env -> Bool
-evalB exp env = evalState (runB exp) env
+evalB :: BExp -> Env -> [Bool]
+evalB exp env = evalStateT (runB exp) env
 
-execStmt :: Stmt -> Env
-execStmt exp = execState (runStmt exp) env
+execStmt :: Stmt -> [Env]
+execStmt exp = execStateT (runStmt exp) env
     where env = (0, 0)
 
-test_arith =    (evalA (Int 42)        (2,  2) == 42)
-             && (evalA Y               (2, 42) == 42)
-             && (evalA (Add (Int 2) Y) (2, 40) == 42)
-             && (evalA (Add X       Y) (2, 40) == 42)
-             && (evalA (Add (Neg X) Y) (2, 44) == 42)
+test_arith =    (evalA (Int 42)        (2,  2) == [42])
+             && (evalA Y               (2, 42) == [42])
+             && (evalA (Add (Int 2) Y) (2, 40) == [42])
+             && (evalA (Add X       Y) (2, 40) == [42])
+             && (evalA (Add (Neg X) Y) (2, 44) == [42])
 
-test_bool =     (evalB (Bool True)    (2, 2) == True)
-             && (evalB (LessThan X Y) (2, 2) == False)
-             && (evalB (LessThan X Y) (2, 3) == True)
-             && (evalB (LessThan X Y) (3, 2) == False)
+test_bool =     (evalB (Bool True)    (2, 2) == [True])
+             && (evalB (LessThan X Y) (2, 2) == [False])
+             && (evalB (LessThan X Y) (2, 3) == [True])
+             && (evalB (LessThan X Y) (3, 2) == [False])
 
 coundToN n = (While (LessThan X $ Int n) $
                     Block [(AssignX (Add X $ Int 1))])
 
-test_stmt =     (execStmt (Block [])         == (0, 0))
-             && (execStmt (AssignX (Int 42)) == (42, 0))
-             && (execStmt (AssignY (Int 42)) == (0, 42))
+test_stmt =     (execStmt (Block [])         == [(0, 0)])
+             && (execStmt (AssignX (Int 42)) == [(42, 0)])
+             && (execStmt (AssignY (Int 42)) == [(0, 42)])
              && (execStmt (Block [
                               (AssignY (Int 43)),
                               (AssignY (Add Y (Neg $ Int 1)))
-                          ])                 == (0, 42))
-             && (execStmt (coundToN 42)      == (42, 0))
+                          ])                 == [(0, 42)])
+             && (execStmt (coundToN 42)      == [(42, 0)])
 
 main :: IO ()
 main = do putStrLn $ "test_arith: " ++ (show test_arith)
