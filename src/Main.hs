486b9ada7994e0f86282df352fabc6a793325ccc
Nishant Rodrigues <nishantjr@gmail.com>
2024-12-02 19:34:33 -0500

    wip

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Control.Monad
 import Control.Monad.State
+import Control.Applicative
 
 -------------------------------------------------------------------------------
 -- Syntax
@@ -10,15 +11,18 @@ data AExp = Int Int
           | X | Y
           | Neg AExp
           | Add AExp AExp
-    deriving Show
+    deriving (Eq, Show)
 
 data BExp = Bool Bool
           | LessThan AExp AExp
           | Flip
+    deriving (Eq, Show)
 
 data Stmt = While BExp Stmts
+          | If BExp Stmts
           | AssignX AExp
           | AssignY AExp
+    deriving (Eq, Show)
 
 type Stmts = [Stmt]
 type Pgm = Stmts
@@ -27,48 +31,81 @@ type Pgm = Stmts
 -- Configuration
 
 data Config = Config { pgm::Pgm, x::Int, y::Int }
+    deriving (Eq, Show)
+
+-------------------------------------------------------------------------------
+-- Rewriting Infrastructure
 
 -- Rewriting is a transition system over this configuration.
 -- Further we use the List monad to represent non-determinism.
 -- Each item in the resulting list is semantically thought of as a disjunct
 -- of configurations.
+
 type Imp = StateT Config []
+
+-- init gives us the initial configuration of the program
+initConfig :: Pgm -> Config
+initConfig pgm = Config {pgm=pgm, x=0, y=0}
+
 
 -- TODO: We use rewriting to [] as an encoding of "stuck" states.
 -- The more semantically correct way of interpreting this is rewriting
--- to "bottom".
+-- to "bottom", as in the evaluation of `assume false`.
 stuck :: Imp a
-stuck = mempty
+stuck = StateT $ \cfg -> []
+
+-- Execute to termination
+execConfig :: Imp ()
+execConfig = StateT f
+     where f :: Config -> [((), Config)]
+           f config = case nexts of
+                              []  -> [((), config)]
+                              _:_ -> concatMap f nexts
+              where nexts = execStateT step config
+
+exec :: Pgm -> [Config]
+exec pgm = execStateT execConfig $ initConfig pgm
 
 
 -------------------------------------------------------------------------------
--- Statements
+-- Top-level rewrites: Rewriting individual statements
 
-next :: Imp ()
-next =      nextAssignX
+-- Each rewrite is represented as a transition over the StateT Monad, that
+-- are then composed in parallel.
+
+step :: Imp ()
+step =      nextAssignX
         <|> nextAssignY
-        <|> nextWhileTrue
-        <|> nextWhileFalse
+        <|> nextWhile
+        <|> nextIf
 
 nextAssignX
-        = do Config{pgm=pgm} <- get
-             case pgm of
+        = do cfg <- get
+             case pgm cfg of
                   (AssignX (Int e)):ss -> do put cfg{pgm=ss, x=e}
+                  (AssignX v)      :ss -> do v' <- runA v
+                                             put cfg{pgm=(AssignX v'):ss}
                   _                    -> stuck
 nextAssignY
-        = do Config{pgm=pgm} <- get
-             case pgm of
+        = do cfg <- get
+             case pgm cfg of
                   (AssignY (Int e)):ss -> do put cfg{pgm=ss, y=e}
+                  (AssignY v)      :ss -> do v' <- runA v
+                                             put cfg{pgm=(AssignY v'):ss}
+                  _                    -> stuck
+nextWhile
+        = do cfg <- get
+             case pgm cfg of
+                  (While cond body):ss -> do put cfg{pgm=(If cond $ body ++ [While cond body]):ss}
                   _                    -> stuck
 
-nextWhileTrue
-        = do Config{pgm=pgm} <- get
-             case pgm of
-                  (While (Bool False) stmts):ss -> do put cfg{pgm=ss}
-                  _                             -> stuck
-runStmt (While cond stmts) = do c <- runB cond
-                                if c then runStmt $ [stmts, (While cond stmts)]
-                                     else return []
+nextIf  = do cfg <- get
+             case pgm cfg of
+                  (If (Bool True) body):ss  -> do put cfg{pgm=body ++ ss}
+                  (If (Bool False) body):ss -> do put cfg{pgm=ss}
+                  (If cond body):ss -> do c' <- runB cond
+                                          put cfg{pgm=(If c' body):ss}
+                  _                 -> stuck
 
 
 -------------------------------------------------------------------------------
@@ -77,8 +114,15 @@ runStmt (While cond stmts) = do c <- runB cond
 runA :: AExp -> Imp AExp
 runA (Int n)        = stuck
 runA (Neg (Int i))  = return $ Int $ -1 * i
+runA (Neg e)        = do e' <- runA e
+                         return $ Neg e'
 runA (Add (Int l) (Int r))
                     = return $ Int $ l + r
+runA (Add (Int l) r)
+                    = do r' <- runA r
+                         return $ Add (Int l) r'
+runA (Add l r)      = do l' <- runA l
+                         return $ Add l' r
 runA X              = do config <- get
                          return $ Int $ x config
 runA Y              = do config <- get
@@ -92,6 +136,11 @@ runB :: BExp -> Imp BExp
 runB (Bool b)       = stuck
 runB (LessThan (Int l) (Int r))
                     = return $ Bool $ l < r
+runB (LessThan (Int l) r)
+                    = do r' <- runA r
+                         return $ LessThan (Int l) r'
+runB (LessThan l r) = do l' <- runA l
+                         return $ LessThan l' r
 runB (Flip)         = lift $ do ret <- [(Bool True), (Bool False)]
                                 return ret
 
@@ -99,49 +148,53 @@ runB (Flip)         = lift $ do ret <- [(Bool True), (Bool False)]
 -------------------------------------------------------------------------------
 -- Testing
 
-evalA :: AExp -> (Int, Int) -> [Int]
-evalA exp xy = evalStateT (runA exp) Config{pgm = [], x = fst xy, y = snd xy}
+--- Execute a program to termination and
+execxy p = map (\c -> (x c, y c)) $ exec p
 
-evalB :: BExp -> (Int, Int) -> [Bool]
-evalB exp xy = evalStateT (runB exp) Config{pgm = [], x = fst xy, y = snd xy}
+execA :: AExp -> (Int, Int) -> [AExp]
+execA exp xy = evalStateT (runA exp) Config{pgm = [], x = fst xy, y = snd xy}
 
-execStmt :: Stmt -> [(Int, Int)]
-execStmt exp = fmap (\r -> (x r, y r)) results
-    where config = Config{pgm = [], x = 0, y = 0}
-          results = execStateT (runStmt exp) config
+execB :: BExp -> (Int, Int) -> [BExp]
+execB exp xy = evalStateT (runB exp) Config{pgm = [], x = fst xy, y = snd xy}
 
-test_arith =    (evalA (Int 42)        (2,  2) == [42])
-             && (evalA Y               (2, 42) == [42])
-             && (evalA (Add (Int 2) Y) (2, 40) == [42])
-             && (evalA (Add X       Y) (2, 40) == [42])
-             && (evalA (Add (Neg X) Y) (2, 44) == [42])
+test_arith =    (execA (Int 42)        (2,  2) == [Int 42])
+             && (execA Y               (2, 42) == [Int 42])
+             && (execA (Add (Int 2) Y) (2, 40) == [Int 42])
+             && (execA (Add X       Y) (2, 40) == [Int 42])
+             && (execA (Add (Neg X) Y) (2, 44) == [Int 42])
 
-test_bool =     (evalB (Bool True)    (2, 2) == [True])
-             && (evalB (LessThan X Y) (2, 2) == [False])
-             && (evalB (LessThan X Y) (2, 3) == [True])
-             && (evalB (LessThan X Y) (3, 2) == [False])
+test_bool =     (execB (Bool True)    (2, 2) == [Bool True])
+             && (execB (LessThan X Y) (2, 2) == [Bool False])
+             && (execB (LessThan X Y) (2, 3) == [Bool True])
+             && (execB (LessThan X Y) (3, 2) == [Bool False])
 
-coundToN n = (While (LessThan X $ Int n) $
-                    Block [(AssignX (Add X $ Int 1))])
+coundToN n = [While (LessThan X $ Int n) $
+                    [(AssignX (Add X $ Int 1))]]
 
-test_stmt =     (execStmt (Block [])         == [(0, 0)])
-             && (execStmt (AssignX (Int 42)) == [(42, 0)])
-             && (execStmt (AssignY (Int 42)) == [(0, 42)])
-             && (execStmt (Block [
-                              (AssignY (Int 43)),
-                              (AssignY (Add Y (Neg $ Int 1)))
-                          ])                 == [(0, 42)])
-             && (execStmt (coundToN 42)      == [(42, 0)])
-
-         ------ The following fails because runStmt is a recursive function
-         ------ that must execute to completion.
-         --- && ((take 5 (execStmt (While (Flip) $
-         ---                        Block [(AssignX (Add X $ Int 1))])))
-         ---                                 == [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0),])
-
-
+test_stmt =    (execxy ([])                 == [(0, 0)])
+            && (execxy ([AssignX (Int 42)]) == [(42, 0)])
+            && (execxy ([AssignY (Int 42)]) == [(0, 42)])
+            && (execxy ([AssignX (Int 42),
+                         AssignY (Int 42)]) == [(42, 42)])
+            && (execxy ([AssignX (Int 42),
+                         AssignY X])        == [(42, 42)] )
+            && (execxy ([ (AssignY (Int 43)),
+                          (AssignY (Add Y (Neg $ Int 1)))
+                        ])                 == [(0, 42)])
+            && (execxy (coundToN 42)      == [(42, 0)])
+-- 
+--          ------ The following fails because runStmt is a recursive function
+--          ------ that must execute to completion.
+--          --- && ((take 5 (execStmt (While (Flip) $
+--          ---                        Block [(AssignX (Add X $ Int 1))])))
+--          ---                                 == [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0),])
+-- 
+-- 
 main :: IO ()
-main = do putStrLn $ "test_arith: " ++ (show test_arith)
-          putStrLn $ "test_bool: " ++ (show test_bool)
+main = do putStrLn $ "test_stmt: " ++ (show $ exec (coundToN 42))
+          putStrLn $ "test_stmt: " ++ (show $ exec $ (pgm . head) $ exec [AssignX (Int 1), AssignY X])
           putStrLn $ "test_stmt: " ++ (show test_stmt)
+          -- putStrLn $ "test_arith: " ++ (show test_arith)
+          -- putStrLn $ "test_bool: " ++ (show test_bool)
 
+-- 
