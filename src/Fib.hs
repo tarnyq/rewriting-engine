ebccd75bbee447426e0ca8ee9afca6de66d86a50
Curt J. Sampson <cjs@cynic.net>
2025-06-07 09:21:51 +0000

    src/Fib: Fix proof and remove verbose output, so we can carry on
    This lets us carry on with other things; we'll come back to
    the WIP commits later.

--- src/Fib.hs
+++ src/Fib.hs
@@ -2,7 +2,6 @@
 -- https://nikivazou.github.io/lh-course/Lecture_05_ProofsPrograms.html
 
 {-# OPTIONS_GHC -fplugin=LiquidHaskell #-}
-{-# OPTIONS_GHC -fplugin-opt=LiquidHaskell:--verbose #-}
 {-@ LIQUID "--reflection" @-}
 
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
@@ -57,7 +56,7 @@ c _ = 9         -- LH can discover that e.g. 'negate 4' does not LH-typecheck
 -- {-@ cP :: { v:Proof | c 1 == 9   } @-}
    {-@ cP :: { v:Proof | c 1 == c 2 } @-}
 cP :: Proof
-cP = c 1 === 8 === c 2 *** QED
+cP = c 1 === 9 === c 2 *** QED
 
 -- This does not work: cP = trival *** QED
 --
