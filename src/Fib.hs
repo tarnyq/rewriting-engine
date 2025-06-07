54e24ad62af7972a7d4eb52b6e692ef9bba77afc
Nishant Rodrigues <nishantjr@gmail.com>
2025-06-07 04:58:47 +0000

    src/Fib: Start testing symbolic execution
    The issue in previous attempts with not being able to find
    Language.Haskell.Liquid.ProofCombinators was that we need
    liquid-prelude not only in stack.yaml `extra-deps`, but also in
    package.yaml `dependencies`.

---
+++ src/Fib.hs
@@ -0,0 +1,26 @@
+-- Testing "symbolic execution" based on:
+-- https://nikivazou.github.io/lh-course/Lecture_05_ProofsPrograms.html
+
+{-# OPTIONS_GHC -fplugin=LiquidHaskell #-}
+{-@ LIQUID "--reflection" @-}
+
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+-- XXX Add this to package.yaml, when we figure out the syntax there.
+{-# LANGUAGE UnicodeSyntax #-}
+
+module Fib () where
+
+import Language.Haskell.Liquid.ProofCombinators
+
+{-@ reflect fib @-}
+{-@ fib :: Nat -> Nat @-}
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+{-@ fibTwo :: () -> { fib 2 = 1 } @-}
+fibTwo :: () -> Proof
+fibTwo _ =   fib 2
+         === fib 1 + fib 0
+                  *** QED
