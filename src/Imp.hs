ecb1a61145f57461f59f5b8600496048e8b07955
Curt J. Sampson <cjs@cynic.net>
2025-06-08 07:29:29 +0000

    WIP Imp: Remainder of syntax, sample program, improved docs
    XXX As develop this, we want an easy way to `show sum_imp` (or other
    things) after a build. (We don't really need to bother with a unit test for
    what show produces because if the `show` works, the type system has taken
    care of almost all the rest.) Can we do this by having Test feed an
    expression into ghci? The more traditional way is to run ghci and manually
    :reload and type in your expression again every time, which is tedious.
    
    TODO in program:
    - Confirm the comment for mkStmt is correct.
    - Clean up sum_imp, especially to remove parens and make use of precedence.
    
    TODO document here:
    - Head comment, refrences
    - StandaloneDeriving
    - 'KImp'
    - Remainder of abstract syntax.
    - Sample program from tutorial
    - mkStmt

--- src/Imp.hs
+++ src/Imp.hs
@@ -1,8 +1,19 @@
 {-  Abstract syntax and semantics for the K tutorial 'Imp' language.
 
-    This would normally be in a separate repo that uses this library,
-    but it's here while we do development on the framework.
+    (This would normally be in a separate repo that uses this library,
+    but it's here while we do development on the framework.)
+
+    This is a translation to KMonad of the K syntax and semantics for the
+    Imp programming language and associated examples from the K
+    Tutorial[1]. There is a separate implementation of Imp in the
+    imp-semantics repo[2] which is not the same.
+
+    [1]: https://github.com/runtimeverification/pl-tutorial/blob/master/1_k/2_imp/lesson_5/imp.md
+    [2]: https://github.com/runtimeverification/imp-semantics
 -}
+
+-- XXX Add these to package.yaml, when we figure out the syntax there.
+{-# LANGUAGE StandaloneDeriving #-}
 
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module Imp () where
@@ -17,6 +28,8 @@ module Imp () where
     actually part of the semantics. (We do however, for convenience,
     keep the precedence settings, both to document them and because it's
     handy when building an AST in Haskell code.)
+
+    Below we refer to the Imp module as written in K as 'KImp'.
 -}
 
 type Id     = String
@@ -26,9 +39,66 @@ data AExp   = Int Integer
             | AExp :/ AExp
          -- | Parens AExp       -- Needed for concrete syntax only.
             | AExp :+ AExp
+data BExp   = Bool Bool
+            | AExp :<= AExp
+            | Not BExp
+         -- | Parens BExp       -- Needed for concrete syntax only.
+            | BExp :&& BExp
+data Block  = StmtBlock Stmt    -- KImp 'Stmt' really should be 'Stmts'. :-/
+            | EmptyBlock
+data Stmt   = Block Block
+            | Id := AExp
+            | If BExp Block Block
+            | While BExp Block
+            | Sequence Stmt Stmt
+data Pgm    = Pgm Ids Stmt
+type Ids    = [Id]
 
-infixl 7 :/                     -- Same precedences as Prelude.Num
-infix  6 :+                     -- Left-assoc in K for more efficient parsing.
+                                -- Same precedence values as the prelude.
+infixl 7  :/
+infix  6  :+                    -- KImp: Left-assoc for more efficient parsing.
+infix  4  :<=
+infixl 3  :&&                   -- Should be RA! But broken this way in KImp.
 
--- Sample program 'sum.int': this essentially serves as our test.
--- (XXX we don't have enough "syntax" for this yet.)
+--  The KImp parser doesn't produce a list of statements, but instead, via
+--  right-associative parsing, produces a left-skewing nearly degenerate
+--  binary tree. (Yes, this is weird, but a consequence of 'Stmt' rather
+--  than 'Block' being the root of the AST; a 'Stmt' must be able to act
+--  like 'Block'.)
+--  We provide this helper function to do the same thing as the KImp parser.
+mkStmt :: [Stmt] -> Stmt
+mkStmt stmtList = program (reverse stmtList) where
+    program []     = error "programs must have at least one statement"
+    program [s]    = s
+    program (s:ss) = Sequence (program ss) s
+
+--  Make all this showable just for convenience and debugging.
+deriving instance Show AExp
+deriving instance Show BExp
+deriving instance Show Block
+deriving instance Show Stmt     -- Not a list, so 'showList' override pointless.
+deriving instance Show Pgm
+
+{----------------------------------------------------------------------
+    Sample program 'sum.imp': this essentially serves as our test.
+
+        int n, sum;
+        n = 100;
+        sum = 0;
+        while (!(n <= 0)) {
+          sum = sum + n;
+          n = n + -1;
+        }
+-}
+sum_imp :: Pgm
+sum_imp = Pgm ids stmt where
+    ids  = ["n", "sum"]
+    stmt = mkStmt
+         [ "n" := Int 100
+         , "sum" := Int 0
+         , While (Not (Var "n" :<= (Int 0)))
+              (StmtBlock (mkStmt
+                [ "sum" := (Var "sum" :+ Var "n")
+                , "n" := (Var "n" :+ Negate 1)
+                ]))
+         ]
