a232866387fa3c952c76702504b68bc82c4179d8
Curt J. Sampson <cjs@cynic.net>
2025-06-08 05:16:50 +0000

    src/BExp: Comment to explain what this is, as it's for reference only
    This is basically just the commit messages from its original commit.

--- src/BExp.hs
+++ src/BExp.hs
@@ -1,3 +1,33 @@
+{-  One try (but wrong) at abstract syntax and semantics for a binary
+    expression langauge.
+
+    Because we're embedding the build of our AST into Haskell, we don't
+    have or need a concrete syntax or associated parser for it; we simply
+    build the AST directly.
+
+    We introduce a State, the equivalent of the K "configuration", that is
+    a record containing fields for what K would call "cells." This is not
+    actually necessary at this point (it's fine to use a State that isn't
+    a record), but reminds us that this is the way K works and we want to
+    support separate names for sub-states as an option.
+
+    The top-level evaluator must take and produce a State; in our case
+    since we have that one field ("cell"), `program`, in it; we simply
+    apply the appropriate evaluator for that field, `peval`. A more
+    sophisticated evaluator would look at other parts of the state as
+    well, to express state external to the program.
+
+    `peval` is a standard recursive evaluator for expressions.
+
+    There are a load of problems with this super-simple approach ,
+    including a lack of modularity, extensibility, inability to handle
+    non-determinism and interleavings, and so on, all of which generalise
+    to a lack of abstraction. We will later be introducing techniques to
+    handle all this, that probably eventually lead to giving a list of
+    rewrite rules to a function that produces an evaluator, or something
+    like that.
+-}
+
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module BExp () where
 
