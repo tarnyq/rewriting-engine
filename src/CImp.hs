f731ecd7398c99b9c2d8bfcb1d82863c8d96a6b4
Curt J. Sampson <cjs@cynic.net>
2025-06-14 09:05:05 +0000

    CImp: Rename Store,State to CImpStore,CImpState etc.
    This makes it clear that these are specific to the CImp semantics,
    which helps when thinking about what we will genercise.

--- src/CImp.hs
+++ src/CImp.hs
@@ -31,7 +31,7 @@ deriving instance Show IExp
 ----------------------------------------------------------------------
 --  Semantics
 
---  A 'Store' contains *all* possible variables each initialised to the
+--  A 'CImpStore' contains *all* possible variables each initialised to the
 --  default value '0'. (For efficiency, we do not actually allocate storage
 --  for these until they are assigned.) This is only one way of doing it
 --  and was chosen because it's the simplest to implement. It also happens
@@ -39,24 +39,24 @@ deriving instance Show IExp
 --  that were assigned. Different implementations might make it e.g. more
 --  difficult to get that but easier to get the set of all variables that
 --  were referenced.
-type Store      = Map.Map Id Integer
-initStore       :: Store
-initStore       = Map.empty
-lookupStore     :: Id -> Store -> Integer
+type CImpStore   = Map.Map Id Integer
+iniCImpStore     :: CImpStore
+iniCImpStore     = Map.empty
+lookupStore     :: Id -> CImpStore -> Integer
 lookupStore     = Map.findWithDefault 0
 
---  The State includes the program; our "program counter" is just removing
+--  The CImpState includes the program; our "program counter" is just removing
 --  each line as it's executed (which is fine when we have no loops).
 --  Whether we use a mutible program here that we rewrite is really just a
 --  decision on the part of the implementer (though this may become less
 --  true as we better define the framework).
-data State  = State
+data CImpState  = CImpState
             {  prog :: Prog
-            , store :: Store
+            , store :: CImpStore
             }  deriving Show
 
-initState :: Prog -> State
-initState prog  = State prog initStore
+initCImpState :: Prog -> CImpState
+initCImpState prog  = CImpState prog iniCImpStore
 
 ----------------------------------------------------------------------
 -- Examples and Tests
@@ -64,16 +64,16 @@ initState prog  = State prog initStore
 one_imp :: Prog
 one_imp = Seq [ 'i' := Const 42, 'j' := Var 'i' ]
 
-run :: Prog -> State
-run prog = evalProg (initState prog)  where
-    evalProg :: State -> State
-    evalProg state@(State (Seq []) _) = state
-    evalProg state@(State (Seq (cmd:cmds)) σ)
+run :: Prog -> CImpState
+run prog = evalProg (initCImpState prog)  where
+    evalProg :: CImpState -> CImpState
+    evalProg state@(CImpState (Seq []) _) = state
+    evalProg state@(CImpState (Seq (cmd:cmds)) σ)
         = evalProg state { prog = Seq cmds, store = evalCmd cmd σ }
 
 -- This cannot rewrite the program in the state, so is used only for
 -- simple Cmds where the rewriting of the program is just the caller
 -- removing the executed command.
-evalCmd :: Cmd -> Store -> Store
+evalCmd :: Cmd -> CImpStore -> CImpStore
 evalCmd (v₁ := Const c₁) store = Map.insert v₁ c₁ store
 evalCmd (v₁ := Var v₂)   store = Map.insert v₁ (lookupStore v₂ store) store
