9ca141a5521c63e2ba468b0b59d412c93bd55bf5
Manasvi Saxena <msaxena2@illinois.edu>
2025-09-15 10:46:36 -0500

    WIP src/CleanImp: adapting "lifts" to MonadRewrite form
    The infrastructure to "lift" nested representation such as AExp etc."
    should be subsumed by the move to MonadRewrite.

---
+++ src/CleanImpTest.hs
@@ -0,0 +1,22 @@
+module CleanImpTest (tests) where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import CleanImp
+import Data.Map (fromList)
+
+tests :: TestTree
+tests = testGroup "CleanImp Tests"
+  [ testCase "SumToN 10" $
+          (eval_imp $ sum_imp 10)
+      @?= (State [] (fromList [("n",0),("sum",55)]))
+  , testCase "div" $
+          (eval_imp divide_imp)
+      @?= (State [] (fromList [("a",100),("b",3),("r",33)]))
+  , testCase "div0" $
+          (eval_imp div0_imp)
+      @?= (State -- Program is stuck since we cannot divide by zero.
+                 [KI_AExp (Int 42 :/ Int 0), KI_Stmt ("r" := AHole), KI_Stmt (Block [])]
+                 (fromList [("r",0)]))
+  ]
--- unit-test/Main.hs
+++ unit-test/Main.hs
@@ -1,7 +1,12 @@
 import Test.Tasty
 
+import qualified CleanImpTest
 import qualified KTutImpTest
 import qualified NonDetTest
 
 main :: IO ()
-main = defaultMain $ testGroup "Tests" [KTutImpTest.tests, NonDetTest.tests]
+main = defaultMain $ testGroup "Tests"
+    [ CleanImpTest.tests
+    , KTutImpTest.tests
+    , NonDetTest.tests
+    ]
