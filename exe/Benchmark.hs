f09be6169d2cfa7b2aef61266e1161106c540d6a
Curt J. Sampson <cjs@cynic.net>
2025-09-17 10:16:04 +0000

    exe/Benchmark: Reformat

--- exe/Benchmark.hs
+++ exe/Benchmark.hs
@@ -1,22 +1,22 @@
 module Main (main) where
 
+import System.Environment (getArgs)
+
 import KTutImp
 import HandOptImp
 
-import System.Environment (getArgs)
-
 main :: IO ()
-main =
-  do args <- getArgs
-     case args of
-       ["imp",     "sum", n]    -> print $ eval_imp    (sum_imp $ read n)
-       ["imp_io",  "sum_io"]    -> do state <- eval_imp_io sum_imp_io
+main = do
+    args <- getArgs
+    case args of
+        ["imp",     "sum", n]   -> print $ eval_imp    (sum_imp $ read n)
+        ["imp_io",  "sum_io"]   -> do state <- eval_imp_io sum_imp_io
                                       print state
-       ["imppure","sum_io", n]  -> print $ eval_imp_io_pure sum_imp_io [n]
-       ["optimp",  "sum", n]    -> print $ eval_hand_opt_imp (sum_imp $ read n)
-       ["summarized",     n]    -> print $ eval_sum_summary $ read n
-
-       -- No args; Run the standard benchmark
-       []                       -> print $ eval_imp $ sum_imp $ 10*1000*1000
-
-       _ -> putStrLn "Bad usage." -- TODO should go to stderr.
+        ["imppure","sum_io", n] -> print $ eval_imp_io_pure sum_imp_io [n]
+        ["optimp",  "sum", n]   -> print $ eval_hand_opt_imp (sum_imp $ read n)
+        ["summarized",     n]   -> print $ eval_sum_summary $ read n
+        -- No args; Run the standard benchmark
+        []                      -> print $ eval_imp $ sum_imp $ 10*1000*1000
+        -- Bad arguments
+        _                       -> do
+            putStrLn "Bad usage." -- TODO should go to stderr.
